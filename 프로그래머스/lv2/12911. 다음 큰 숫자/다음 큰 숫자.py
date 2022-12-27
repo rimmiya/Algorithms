8407ba7a1b07b20e1517b498e9f1469ef545ad06
Yerim Seok <43161401+rimmiya@users.noreply.github.com>
2022-12-28 00:25:39 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,9 +1,8 @@
 def solution(n):
-    answer = n+1
+    answer = n
     n_b = format(n, 'b').count('1')
 
     while(1):
+        answer += 1
         if n_b == format(answer, 'b').count('1'):
             return answer
-        else:
-            answer += 1
