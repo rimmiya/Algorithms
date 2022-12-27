f307617aa72a7b1435d5c98ad7d365698d8ffccf
Yerim Seok <43161401+rimmiya@users.noreply.github.com>
2022-12-28 01:00:04 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,19 +1,11 @@
-import math
-
 def solution(n, words):
-    answer = [0,0]
     used = []
-
+    
     for i, w in enumerate(words):
         if not used:
             used.append(w)
             continue
         if (w in used) or (used[-1][-1] != w[0]):
-            if (i+1) % n == 0:
-                answer[0] = n
-            else:
-                answer[0] = (i+1) % n
-            answer[1] = math.ceil((i+1) / n)
-            break
+            return [(i % n) + 1, (i // n) + 1]
         used.append(w)
-    return answer
+    return [0, 0]
