15932adba1122a4b5ef9ae2f79893be97ed4c5bc
Yerim Seok <43161401+rimmiya@users.noreply.github.com>
2022-12-28 00:21:14 +0900

    [level 2] Title: 피보나치 수, Time: 23.51 ms, Memory: 13.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -2,6 +2,6 @@ def solution(n):
     fib = [0, 1]
     
     for i in range(2, n+1):
-        fib.append(fib[i-1] + fib[i-2])
+        fib.append(((fib[i-1] % 1234567) + (fib[i-2] % 1234567)) % 1234567)
     
-    return fib[n] % 1234567
+    return fib[-1]
