939f0a99e4a46e07f9d1eafd4995490a586f39bb
Yerim Seok <43161401+rimmiya@users.noreply.github.com>
2022-12-28 01:54:06 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,11 +1,13 @@
-def gcd(a, b):
-    while b > 0:
-        a, b = b, a % b
-    return a
+# def gcd(a, b):
+#     while b > 0:
+#         a, b = b, a % b
+#     return a
+
+from math import gcd
 
 def solution(arr):
     answer = arr[0]
 
     for i in range(len(arr)):
-        answer = (answer * arr[i]) / gcd(answer, arr[i])
+        answer = (answer * arr[i]) // gcd(answer, arr[i])
     return answer
