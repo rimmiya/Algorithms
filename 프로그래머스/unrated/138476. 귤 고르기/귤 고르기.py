27b336fe7c7aed5031a0ab66190f808118cad199
Yerim Seok <43161401+rimmiya@users.noreply.github.com>
2023-01-01 18:07:51 +0900

    [unrated] Title: 귤 고르기, Time: 39.44 ms, Memory: 25.8 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -4,14 +4,15 @@ def solution(k, tangerine):
     size = {}
     
     for t in tangerine:
-        if t in size.keys():
-            size[t] += 1
-        else:
-            size[t] = 1
+        if t not in size:
+            size[t] = 0
+        size[t] += 1
+        
     size = dict(sorted(size.items(), key = lambda x : x[1], reverse = True))
+    cnt = 0
     
     for key in size.keys():
-        k = k - size[key]
+        cnt += size[key]
         answer += 1
-        if k <= 0:
+        if cnt >= k:
             return answer
