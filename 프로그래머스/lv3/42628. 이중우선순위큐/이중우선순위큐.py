de0db40420a0850a73164b779d27cdd56cb3a8a4
Yerim Seok <43161401+rimmiya@users.noreply.github.com>
2023-01-01 16:10:18 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+def solution(operations):
+    answer = []
+    
+    max_queue = deque()
+    
+    for i in operations:
+        command = i.split()
+        if command[0] == "I":
+            max_queue.append(int(command[1]))
+            max_queue = deque(sorted(max_queue, reverse = True))
+        elif command[0] == "D":
+            if max_queue:
+                if command[1] == "1":
+                    max_queue.popleft()
+                elif command[1] == "-1":
+                    max_queue.pop()
+        
+    if not max_queue:
+        return [0,0]
+    else:
+        return [max_queue.popleft(), max_queue.pop()]
