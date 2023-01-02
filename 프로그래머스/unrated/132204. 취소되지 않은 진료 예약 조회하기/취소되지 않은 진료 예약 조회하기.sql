9a79b02e24ea18dcb4a41545099245f276c77c8a
Yerim Seok <43161401+rimmiya@users.noreply.github.com>
2023-01-02 18:12:11 +0900

    [unrated] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -0,0 +1,12 @@
+SELECT A.APNT_NO, P.PT_NAME, P.PT_NO, A.MCDP_CD, D.DR_NAME, A.APNT_YMD 
+FROM APPOINTMENT AS A
+JOIN PATIENT AS P
+ON P.PT_NO = A.PT_NO
+JOIN DOCTOR AS D
+ON A.MDDR_ID = D.DR_ID
+WHERE A.APNT_YMD LIKE "2022-04-13%" AND A.APNT_CNCL_YN = 'N' AND A.MCDP_CD = "CS"
+ORDER BY A.APNT_YMD
+
+# 2022년 4월 13일 취소되지 않은 흉부외과(CS) 진료 예약 내역을 조회
+# 진료예약번호, 환자이름, 환자번호, 진료과코드, 의사이름, 진료예약일시
+# 진료예약일시를 기준으로 오름차순
