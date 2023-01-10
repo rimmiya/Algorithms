2048878deab75619fdaaa7ad46365d39c09e36ac
Yerim Seok <43161401+rimmiya@users.noreply.github.com>
2023-01-10 14:01:30 +0900

    [level 2] Title: 주식가격, Time: 16.09 ms, Memory: 17.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -0,0 +1,10 @@
+def solution(prices):
+    answer = [i for i in range(len(prices) -1, -1, -1)]
+    stack = [0]
+    
+    for i in range(1, len(prices)):
+        while stack and prices[stack[-1]] > prices[i]:
+            j = stack.pop()
+            answer[j] = i - j
+        stack.append(i)
+    return answer
