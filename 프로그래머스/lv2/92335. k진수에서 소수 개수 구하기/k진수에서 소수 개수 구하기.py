fe55f1f68330346f3589e3895742d0a6b4960b22
Yerim Seok <43161401+rimmiya@users.noreply.github.com>
2023-01-11 23:05:21 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -1,30 +1,25 @@
 import math
 
-def prime(num):
+def isprime(num):
+    if num == 1: return False
+
     for i in range(2, int(math.sqrt(num)) + 1):
         if num % i == 0:
             return False
     return True
 
+def conv(n, k):
+    s = ''
+    while n:
+        s = str(n%k) + s
+        n //= k
+    return s
+
 def solution(n, k):
     answer = 0
-    num = ""
+    num = list(conv(n, k).split("0"))
     
-    while n > 0:
-        r = n % k
-        if r == 0 and n > k:
-            if len(num) > 0 and int(num) != 1 and prime(int(num)):
-                answer += 1
-            num = ""
-        if r != 0 and n > k:
-            num = str(r) + num
-        if r == 0 and n < k:
-            if len(num) > 0 and int(num) != 1 and prime(int(num)):
-                answer += 1
-        elif r != 0 and n < k:
-            num = str(n) + num
-            if prime(int(num)):
-                answer += 1
-                return answer
-        n //= k
+    for i in num:
+        if not i: continue
+        elif isprime(int(i)): answer += 1
     return answer
