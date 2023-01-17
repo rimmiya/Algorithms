3eb19a6db8f3be69fec1e18f3abfaf45d586d1c2
Yerim Seok <43161401+rimmiya@users.noreply.github.com>
2023-01-17 20:21:10 +0900

    [level 2] Title: 타겟 넘버, Time: 6.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,34 +1,22 @@
 # BFS
-# def solution(numbers, target):
-#     answer = 0
-    
-#     bfs = [[numbers[0], 0], [-1 * numbers[0], 0]]
-    
-#     while bfs:
-#         num, idx = dfs.pop()
-#         idx += 1
-#         if idx < len(numbers):
-#             bfs.append([num + numbers[idx], idx])
-#             bfs.append([num - numbers[idx], idx])
-#         else:
-#             if num == target:
-#                 answer += 1
-#     return answer
+from collections import deque
 
-# DFS
 def solution(numbers, target):
-    answer = dfs(numbers, target, 0)
-    return answer 
-
-def dfs(numbers, target, depth):
     answer = 0
-    if depth == len(numbers):
-        if sum(numbers) == target:
-            return 1
+    
+    if sum(numbers) == target:
+        return 1
+    
+    queue = [[numbers[0], 0], [-numbers[0], 0]]
+    
+    while queue:
+        num, idx = queue.pop()
+        idx += 1
+        if idx < len(numbers):
+            queue.append([num + numbers[idx], idx])
+            queue.append([num - numbers[idx], idx])
         else:
-            return 0
-    else:
-        answer += dfs(numbers, target, depth+1)
-        numbers[depth] *= -1
-        answer += dfs(numbers, target, depth+1)
+            if num == target:
+                answer += 1
     return answer
+        
