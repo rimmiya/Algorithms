f00a2d6aa64a305fc647662b6f86648230198452
Yerim Seok <43161401+rimmiya@users.noreply.github.com>
2023-01-18 14:53:44 +0900

    [level 2] Title: 구명보트, Time: 10.01 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.py
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -1,15 +1,23 @@
 def solution(people, limit):
     answer = 0
-    people.sort()
-    s = 0
-    e = len(people) - 1
-    
-    while(s <= e):
-        if (len(people) >= 2) and (people[s] + people[e] <= limit):
+    people = sorted(people)
+    boat = 0
+    i = 0
+    j = len(people) - 1
+    while i < j:
+        boat += people[j]
+        if limit - boat < people[i]:
             answer += 1
-            s += 1
-            e -= 1
+            j -= 1
         else:
-            answer += 1
-            e -= 1
+            while i < j:
+                boat += people[i]
+                i += 1
+                if limit - boat < people[i]:
+                    answer += 1
+                    j -= 1
+                    break
+        boat = 0
+    if i == j:
+        answer += 1
     return answer
