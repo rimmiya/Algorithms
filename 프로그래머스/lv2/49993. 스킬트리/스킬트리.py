0e287ab4e283f03564a1113ad0bc743a90afaddb
Yerim Seok <43161401+rimmiya@users.noreply.github.com>
2023-01-24 01:17:38 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
@@ -2,14 +2,13 @@ def solution(skill, skill_trees):
     answer = 0
     
     for tree in skill_trees:
-        can_tree = []
+        i = 0
         for t in tree:
             if t in skill:
-                can_tree.append(skill.index(t))
-
-        for i in range(len(can_tree)):
-            if i != can_tree[i]:
-                break
+                if skill[i] != t:
+                    break
+                else:
+                    i +=1
         else:
             answer += 1
     return answer
