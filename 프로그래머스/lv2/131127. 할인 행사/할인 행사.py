6cb21cdcb7aa5e5d562d3d6f320ccce0fa0b1553
Yerim Seok <43161401+rimmiya@users.noreply.github.com>
2023-01-24 16:28:37 +0900

    [level 2] Title: 할인 행사, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
@@ -5,7 +5,7 @@ def solution(want, number, discount):
     can = {}
     for i in range(len(want)):
         can[want[i]] = number[i]
-    for i in range(len(discount)):
+    for i in range(len(discount)-9):
         tmp = dict(Counter(discount[i:10+i]))
         if can == tmp:
             answer += 1
